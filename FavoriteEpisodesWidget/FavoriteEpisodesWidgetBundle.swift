99ba80e55b0b591eb8d1ac8449faa377cbaa21ad
Han <csh771355@gmail.com>
2024-12-26 14:12:59 +0800

    feat: add FavoriteEpisodesWidget target

---
+++ FavoriteEpisodesWidget/FavoriteEpisodesWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  FavoriteEpisodesWidgetBundle.swift
+//  FavoriteEpisodesWidget
+//
+//  Created by Han Chen on 2024/12/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct FavoriteEpisodesWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        FavoriteEpisodesWidget()
+    }
+}
